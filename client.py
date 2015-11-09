ceddcecf13159ffb031b06608328b43e6eca8336
Sebastian Łasisz <slasisz@gmail.com>
2015-11-09 16:55:21 +0100

    Added support for sending multiple messages through TCP/IP connection.

--- client.py
+++ client.py
@@ -1,11 +1,25 @@
 # client.py
 import socket
 
-s = socket.socket()
-
 host = socket.gethostname()
 port = 1234
 
-s.connect((host, port))
-s.send("hello world")
-print s.recv(1024)
+try:
+    # while True:
+    for x in range(0, 4):
+            s = socket.socket()
+            s.connect((host, port))
+            # Here will be deframing videos
+            s.send("hello world")
+            print s.recv(4096)
+            # End of deframing video
+            s.close()
+    # Send message to stop the server
+    s = socket.socket()
+    s.connect((host, port))
+    s.send("stop")
+    print s.recv(4096)
+    s.close()
+
+except socket.error:
+    print 'Server is not responding. Shutting down.'
--- server.py
+++ server.py
@@ -6,11 +6,20 @@ host = socket.gethostname()
 port = 1234
 s.bind((host, port))
 
-s.listen(5)
-
+s.listen(1)
+collection = []
 while True:
+    # get all data
     c, addr = s.accept()
-    print c.recv(1024)
-    print 'Got connection from', addr
-    c.send('Thank you for your connecting')
-    c.close()
+    data = c.recv(4096)
+    if data == "stop":
+        c.send('Stopping the server.')
+        break
+    else:
+        collection.append(data)
+        print 'Got connection from', addr
+        c.send('Received the message.')
+c.close()
+
+for c in collection:
+    print c
