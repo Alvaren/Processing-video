5aa25761bc5e85bb3693bc35cdc3098e3a75943e
Sebastian Łasisz <slasisz@gmail.com>
2015-11-09 15:34:46 +0100

    Added TCP/IP Connection through sockets.

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
-__author__ = 'Sedi'
+# client.py
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+port = 1234
+
+s.connect((host, port))
+s.send("hello world")
+print s.recv(1024)
--- server.py
+++ server.py
@@ -1,1 +1,16 @@
-__author__ = 'Sedi'
+# server.py
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 1234
+s.bind((host, port))
+
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+    print c.recv(1024)
+    print 'Got connection from', addr
+    c.send('Thank you for your connecting')
+    c.close()
