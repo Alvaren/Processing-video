75980fd55cd54aca09fe845d1206a2b7f96a013c
Sebastian Łasisz <slasisz@gmail.com>
2015-12-09 18:11:57 +0100

    Fixing shell script.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for i in client server receiver statistics video_retriever; do python ~/documents/repositories/processing-video/$i.py & done
+for i in client server receiver statistics video_retriever; do python $i.py & done
 #start "" %CD%/graphs/final.svg
 # printf "\n Video has been processed. \n"
 $SHELL
